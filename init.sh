c462bec905689173e8d20fe611dade8998cb9004
walid guetarni <guetarni.walid@gmail.com>
2024-03-24 16:03:16 +0100

    Temporary commit

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-echo "En attente de la base de données..."
+echo "Waitning database..."
 while ! nc -z db 3306; do
   sleep 1
 done
 
-echo "La base de données est prête!"
+echo "Database is ready!"
 node ace migration:run
 pm2-runtime start ecosystem.config.js
